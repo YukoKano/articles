2610cdff53a53181c97f8d4363e53eba50938b12
YukoKano <halloween_1031@hotmail.co.jp>
2024-11-21 09:50:55 +0900

    emotionの導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,10 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+const style = css`
+  color: hotpink;
+`;
+
 export default function Home() {
-  return <>hoge</>;
+  return <p css={style}>hoge</p>;
 }

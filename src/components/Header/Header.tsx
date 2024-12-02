bb2a30c56825f25e0f686891ba7b3f87323056e0
YukoKano <halloween_1031@hotmail.co.jp>
2024-12-02 23:35:22 +0900

    mode切り替えスイッチをHeaderに持っていく

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,26 @@
+import { ThemeContext } from "@/pages/_app";
 import { css } from "@emotion/react";
+import { useContext } from "react";
 
-const header = css`
-  padding: 16px;
+// CSSカスタムプロパティで管理した方が記述増えるの避けられるかなぁ
+const header = (mode: "light" | "dark") => css`
+  display: flex;
+  padding: 16px 32px;
+  gap: 16px;
+  justify-content: space-between;
+  align-items: center;
+  background-color: ${mode === "dark" ? "#5e5e5e" : "#fdfdfd"};
 `;
 
 export const Header = () => {
+  const themeContext = useContext(ThemeContext);
+  if (!themeContext) return null;
+
+  const { mode, toggleMode } = themeContext;
   return (
-    <header css={header}>
+    <header css={header(mode)}>
       <h1>header</h1>
+      <button onClick={toggleMode}>モード切り替え</button>
     </header>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,28 +7,29 @@ import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 
 type ThemeMode = "light" | "dark";
-export const ThemeContext = createContext<ThemeMode>("light");
+type ThemeContextType = { mode: ThemeMode; toggleMode: () => void };
+export const ThemeContext = createContext<ThemeContextType | undefined>(
+  undefined
+);
 
 export default function App({ Component, pageProps }: AppProps) {
   const [mode, setMode] = useState<ThemeMode>("light");
 
   const toggleMode = () => {
     setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
-    console.log(mode); // これでコンソール出力されます
+    console.log(mode);
   };
 
   const themeStyles = css`
     background-color: ${mode === "dark" ? "#2e2e2e" : "#fdfdfd"};
     color: ${mode === "dark" ? "#fdfdfd" : "#2e2e2e"};
-    transition: background-color 0.5s, color 0.5s;
     min-height: 100vh;
   `;
 
   return (
-    <ThemeContext.Provider value={mode}>
+    <ThemeContext.Provider value={{ mode, toggleMode }}>
       <div css={themeStyles}>
         <Header />
-        <button onClick={toggleMode}>モード切り替え</button>
         <Component {...pageProps} />
         <Footer />
       </div>

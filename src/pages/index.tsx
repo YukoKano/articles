bfc5aa10852a4fbc0054334003bbd573ca567d44
YukoKano <halloween_1031@hotmail.co.jp>
2024-12-12 23:14:33 +0900

    gsapを利用したアニメーションを試す

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
+import { gsap } from "gsap";
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(ScrollTrigger);
+
 import { css } from "@emotion/react";
 import Image from "next/image";
 
@@ -7,6 +15,7 @@ const wrap = css`
 
 const content = css`
   padding: 5%;
+  transform-style: preserve-3d; /* 子要素の3D効果を保持 */
 `;
 
 const contentImage = css`
@@ -20,6 +29,7 @@ const innerWrap1 = css`
   background-image: url("/image1.jpg");
   background-size: cover;
   background-position: center;
+  transform-style: preserve-3d; /* 子要素の3D効果を保持 */
 `;
 
 const innerText = css`
@@ -102,9 +112,59 @@ const sampleText = css`
 `;
 
 export default function Home() {
+  const testRef = useRef();
+  const firstContainer = useRef();
+
+  useGSAP(
+    () => {
+      const ref = testRef.current;
+      const firstWrap = firstContainer.current;
+      // gsap code here...
+      // gsap.fromTo(
+      //   testRef.current,
+      //   { opacity: 0, x: -200, y: -300 }, // 開始状態
+      //   {
+      //     opacity: 1,
+      //     x: 0,
+      //     y: 0,
+      //     duration: 1,
+      //     scrollTrigger: {
+      //       trigger: testRef.current,
+      //       start: "top 50%", // アニメーションの開始位置
+      //       end: "top 20%", // アニメーションの終了位置
+      //       scrub: true, // スクロールに同期
+      //       markers: true, // デバッグ用マーカー
+      //     },
+      //   }
+      // ); // <-- automatically reverted
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: firstWrap,
+          start: "center center",
+          end: "bottom center",
+          // pin: true, // コンテナを固定
+          scrub: true, // スクロールに同期
+          markers: true, // デバッグ用マーカー
+        },
+      });
+      tl.to(ref, { rotationZ: 3, scale: 1, y: 0, z: 2000, duration: 1 })
+        .to(
+          firstWrap,
+          { rotationZ: -3, scale: 0.9, y: 100, z: -1200, duration: 1 },
+          "<"
+        ) // "<"で同時スタートを指定
+        .to(ref, { rotationZ: 0, y: -500, z: 0, duration: 1 })
+        .to(
+          firstWrap,
+          { rotationZ: -3, scale: 0.6, y: 500, z: 0, duration: 1 },
+          "<"
+        );
+    },
+    { scope: testRef }
+  ); // <-- scope for selector text (optional)
   return (
     <div css={wrap}>
-      <div css={content}>
+      <div css={content} ref={firstContainer}>
         <h2 css={title}>title</h2>
         <div css={contentWrap}>
           <p>text</p>
@@ -119,7 +179,7 @@ export default function Home() {
           />
         </div>
       </div>
-      <div css={innerWrap1}>
+      <div css={innerWrap1} ref={testRef}>
         <div css={innerText}>
           <p>text</p>
           <p style={{ marginTop: "24px" }}>text</p>
